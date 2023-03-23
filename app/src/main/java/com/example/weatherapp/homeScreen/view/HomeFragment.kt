89a3ef3b92f575232e2956ee4536dbfe62940dbb
Clara Mounir Adly <claramounir555@gmail.com>
2023-03-23 20:38:54 +0200

    handle bottom navigation

--- app/src/main/java/com/example/weatherapp/homeScreen/view/HomeFragment.kt
+++ app/src/main/java/com/example/weatherapp/homeScreen/view/HomeFragment.kt
@@ -23,6 +23,8 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
-
+    companion object {
+        fun newInstance() = HomeFragment()
+    }
 
 }
---
+++ app/src/main/java/com/example/weatherapp/homeScreen/view/HourAdapter.kt
@@ -0,0 +1,6 @@
+package com.example.weatherapp.homeScreen.view
+
+import androidx.fragment.app.Fragment
+
+class HourAdapter(private val fragment: Fragment) {
+}
