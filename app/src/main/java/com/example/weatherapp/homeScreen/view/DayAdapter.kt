277135a07ccfd7968668d1c40f9255fa2e70c595
Clara Mounir Adly <claramounir555@gmail.com>
2023-03-25 15:13:51 +0200

    daily adapter logic

--- app/src/main/java/com/example/weatherapp/homeScreen/view/DayAdapter.kt
+++ app/src/main/java/com/example/weatherapp/homeScreen/view/DayAdapter.kt
@@ -1,4 +1,33 @@
 package com.example.weatherapp.homeScreen.view
 
-class DayAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.example.weatherapp.databinding.RowDaysBinding
+import com.example.weatherapp.model.Daily
+
+class DayAdapter(private val fragment: Fragment) : RecyclerView.Adapter<DayAdapter.ViewHolder>() {
+    private var days = emptyList<Daily>()
+    fun setWeatherDay(weatherDays: List<Daily>) {
+        this.days = weatherDays
+        notifyDataSetChanged()
+    }
+    class ViewHolder(val binding: RowDaysBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder( RowDaysBinding.inflate( LayoutInflater.from(parent.context), parent, false))
+
+    }
+
+
+    override fun getItemCount(): Int {
+      return days.size-1
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
