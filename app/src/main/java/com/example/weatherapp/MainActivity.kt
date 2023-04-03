5c26b64368c091b15c51abd5f05d4fe4f636a26c
Clara Mounir Adly <claramounir555@gmail.com>
2023-04-03 17:32:34 +0200

    splash screen animation

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.weatherapp
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.example.weatherapp.databinding.ActivityMainBinding
@@ -18,18 +23,27 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
-    lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
 
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    val navView: BottomNavigationView = binding.bottomNavigation
 
         navController = Navigation.findNavController(this, R.id.nav_host_fragment)
         setupWithNavController(binding.bottomNavigation, navController)
 
+        navController.addOnDestinationChangedListener { _, nd: NavDestination, _ ->
+            if (nd.id == R.id.homeFragment || nd.id == R.id.favoriteFragment || nd.id == R.id.settingFragment
+                || nd.id == R.id.alertFragment || nd.id == R.id.detailsFragment) {
+              navView.visibility = View.VISIBLE
+            } else {
+        navView.visibility = View.GONE
+            }
+        }
     }
 
- 
+
+
 }
